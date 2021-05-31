edef8cd70fa00c9148093d61d2c0d44eebf4d801
FlashBangKev <kevin@getkev.co.uk>
2021-05-31 20:04:40 +0100

    added local storage functionality in js

--- app.js
+++ app.js
@@ -1,23 +1,65 @@
 const container = document.querySelector('.container');
-const seat = document.querySelectorAll('.row .seat:not(.occupied)');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie')
 
+
+
 let ticketPrice = +movieSelect.value;
+
+// Save movie index and price in local storage
+const setMovieData = (movieIndex, moviePrice) => {
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+
+}
 
 // Update total and count
 const updateSelectedCount = () =>{
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+    const seatsIndex = [...selectedSeats].map((seat)=>{
+    return [...seats].indexOf(seat);
+})
+
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
     const selectedSeatsCount = selectedSeats.length;
     count.innerText = selectedSeatsCount;
     total.innerText = selectedSeatsCount * ticketPrice;
 }
 
+// Get data from local storage and populate UI
+
+const populateUI = () => {
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+    if(selectedSeats !== null && selectedSeats.length > 0) {
+        seats.forEach((seat, index) =>{
+            if(selectedSeats.indexOf(index) > -1) {
+                seat.classList.add('selected');
+
+            }
+         
+        })
+
+    }
+
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+    if(selectedMovieIndex !== null) {
+        movieSelect.selectedIndex = selectedMovieIndex;
+    }
+
+}
+
+populateUI();
+
 
 // Movie selection event
 movieSelect.addEventListener('change', (e)=>{
     ticketPrice = e.target.value;
+    setMovieData(e.target.selectedIndex, e.target.value);
     updateSelectedCount();
 
 })
@@ -35,3 +77,8 @@ container.addEventListener('click', (e) => {
     }
 
 })
+
+
+// Initial count and total set
+
+updateSelectedCount();
